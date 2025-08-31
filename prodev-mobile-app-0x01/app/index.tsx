0b428bf4a5db2ed9a2224129d7d7e6bb6d501ec8
SoftwareEngineerGemechu <gemechu.bekele.berga@gmail.com>
2025-08-31 23:03:22 +0300

    Fix and solve some error

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,12 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
-export default function EntryScreen() {
+export default function Index() {
     return (
         <View style={styles.container}>
             <Text>Entry Screen - Awesome</Text>
+
             <View>
-                <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-                <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+                <Text style={styles.largeText}>
+                    Typescript is great if you practice more
+                </Text>
+                <Text style={styles.mediumText}>
+                    React Native provides you a single codebase for cross platforms
+                </Text>
                 <Text style={styles.smallText}>ALX is awesome</Text>
             </View>
         </View>
@@ -15,29 +20,30 @@ export default function EntryScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#90caf9',
+        backgroundColor: "#90caf9",
+        flex: 1, // makes it take the full screen
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
     },
     largeText: {
         fontSize: 30,
-        color: '#f44336',
+        color: "#f44336",
         marginBottom: 5,
-        fontWeight: '700',
-        fontVariant: ['small-caps'],
+        fontWeight: "700",
+        fontVariant: ["small-caps"],
     },
     mediumText: {
         fontSize: 20,
-        color: '#9c27b0',
+        color: "#9c27b0",
         marginBottom: 10,
-        fontWeight: '500',
-        textAlign: 'right',
+        fontWeight: "500",
+        textAlign: "right",
     },
     smallText: {
         fontSize: 15,
-        color: '#2196f3',
-        fontWeight: '400',
-        textAlign: 'center',
+        color: "#2196f3",
+        fontWeight: "400",
+        textAlign: "center",
     },
 });
